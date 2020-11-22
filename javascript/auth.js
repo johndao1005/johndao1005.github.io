32c3030e80aeea552bac44b2a91df17248922519
John Dao <john.d1729@gmail.com>
2020-11-22 22:31:46 +1300

    fixing thing

--- javascript/auth.js
+++ javascript/auth.js
@@ -97,18 +97,3 @@ loginForm.addEventListener('submit', e => {
         loginForm.querySelector('.error').innerHTML = err.message
     })
 })
-
-signupForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-    auth.createUserWithEmailAndPassword(email, password).then(() => {
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.querySelector('.error').innerHTML = '';
-        signupForm.reset();
-
-    }).catch(err => {
-        signupForm.querySelector('.error').innerHTML = err.message
-    })
-})
