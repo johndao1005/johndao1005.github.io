9881fe3e9614b139fbbb9972e5b9e07a144ae79f
John Dao <john.d1729@gmail.com>
2020-11-23 14:11:16 +1300

    validating html code

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/* Change the width of of side nav to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
 //open header and footer html 
 
 //
