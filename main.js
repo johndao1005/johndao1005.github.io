6e92a30bff0aa5070e913de7c4eec00e815d40c3
johndao1005 <john.d1729@gmail.com>
2021-06-24 11:48:57 +1200

    adding more package

--- main.js
+++ main.js
@@ -1,7 +1,70 @@
-const lightbox = GLightbox({
-    'href': 'https://www.youtube.com/watch?v=Ga6RYejo6Hk',
-    'type': 'video',
-    'source': 'youtube', //vimeo, youtube or local
-    'width': 900,
-    'autoplayVideos': 'true',
-})
+(function() {
+    //ANCHOR Easy selector helper function
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    // ANCHOR Adding event listener functions
+    const on = (type, el, listener, all = false) => {
+            let selectEl = select(el, all)
+            if (selectEl) {
+                if (all) {
+                    selectEl.forEach(e => e.addEventListener(type, listener))
+                } else {
+                    selectEl.addEventListener(type, listener)
+                }
+            }
+        }
+        //scroll to an element smooth
+    const scrollto = (el) => {
+            let header = select('#header')
+            let offset = header.offsetHeight
+
+            let elementPos = select(el).offsetTop
+            window.scrollTo({
+                top: elementPos - offset,
+                behavior: 'smooth'
+            })
+        }
+        // ANCHOR adding glight box
+    const lightbox = GLightbox({
+        'href': 'https://www.youtube.com/watch?v=Ga6RYejo6Hk',
+        'type': 'video',
+        'source': 'youtube', //vimeo, youtube or local
+        'width': 900,
+        'autoplayVideos': 'true',
+    })
+
+    //ANCHOR back to top button hiding thelet backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    //ANCHOR Skills animation
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+        new Waypoint({
+            element: skilsContent,
+            offset: '80%',
+            handler: function(direction) {
+                let progress = select('.progress .progress-bar', true);
+                progress.forEach((el) => {
+                    el.style.width = el.getAttribute('aria-valuenow') + '%'
+                });
+            }
+        })
+    }
+})()
